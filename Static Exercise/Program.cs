14d0162ed0948ce2b606778eb8ea9bad519e5344
jon conner <jconnergoldleafpres@gmail.com>
2022-07-18 07:19:00 -0700

    Add project files.

---
+++ Static Exercise/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Static_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = Temp_Converter.FahrenheightToCelsius(68);
+            var fahrenheight = Temp_Converter.CelsiusTorFahrenheight(20);
+
+            Console.WriteLine("After Conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheight {fahrenheight}");
+            
+        }
+    }
+}
---
+++ Static Exercise/Temp Converter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Exercise
+{
+    public static class Temp_Converter
+    {
+        public static double FahrenheightToCelsius(double fahrenheight)
+        {
+            return (fahrenheight - 32) / 1.8;
+        }
+        public static double CelsiusTorFahrenheight(double celsius)
+        {
+            return (celsius * 9) / 5 + 32; 
+        }
+    }
+}
